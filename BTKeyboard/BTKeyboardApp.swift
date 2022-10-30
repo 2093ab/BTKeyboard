bfd1e83f3457901fabb01a230053dbf8a98f6d35
2093ab <gil2093ab@gmail.com>
2022-10-30 19:01:59 +0900

    Initial Commit

---
+++ BTKeyboard/BTKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTKeyboardApp.swift
+//  BTKeyboard
+//
+//  Created by 길수민 on 2022/10/30.
+//
+
+import SwiftUI
+
+@main
+struct BTKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
